0956e9442d2f618b36e0ac6bb458685213f0daae
landlessdrunk <admin@drnk.org>
2026-04-30 21:31:49 -0500

    Commenting out twitter listener.

--- src/listeners/twitter.js
+++ src/listeners/twitter.js
@@ -1,26 +1,26 @@
-import { Listener } from '@sapphire/framework';
-import { Events } from 'discord.js';
+// import { Listener } from '@sapphire/framework';
+// import { Events } from 'discord.js';
 
-export class TwitterListener extends Listener {
-  constructor(context, options) {
-    super(context, {
-      ...options,
-      event: Events.MessageCreate
-    });
-  }
+// export class TwitterListener extends Listener {
+//   constructor(context, options) {
+//     super(context, {
+//       ...options,
+//       event: Events.MessageCreate
+//     });
+//   }
 
-  async run(message) {
-    if (message.author.bot) return;
+//   async run(message) {
+//     if (message.author.bot) return;
 
-    const urlPattern = /https?:\/\/(www\.)?(twitter\.com|x\.com)\/\S+/gi;
-    const matches = message.content.match(urlPattern);
-    if (!matches) return;
+//     const urlPattern = /https?:\/\/(www\.)?(twitter\.com|x\.com)\/\S+/gi;
+//     const matches = message.content.match(urlPattern);
+//     if (!matches) return;
 
-    const vxLinks = matches.map(link => {
-      const url = new URL(link);
-      return new URL(url.pathname, 'https://fixupx.com').toString();
-    });
+//     const vxLinks = matches.map(link => {
+//       const url = new URL(link);
+//       return new URL(url.pathname, 'https://fixupx.com').toString();
+//     });
 
-    message.reply({ content: vxLinks.join('\n') });
-  }
-}
+//     message.reply({ content: vxLinks.join('\n') });
+//   }
+// }
